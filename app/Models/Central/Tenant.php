2e4b6c95cfe181b8c4a2358526bc6827503235a1
Martin <martin.njau@live.com>
2026-05-21 15:58:36 +0000

    May 21, 2026, 6:58:36 PM

--- app/Models/Central/Tenant.php
+++ app/Models/Central/Tenant.php
@@ -4,20 +4,41 @@ declare(strict_types=1);
 
 namespace App\Models\Central;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Auditable;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDomains;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 
-final class Tenant extends BaseTenant implements TenantWithDatabase
+final class Tenant extends BaseTenant implements AuditableContract, TenantWithDatabase
 {
-    use HasDatabase;
-    use HasDomains;
+    use Auditable, HasDatabase, HasDomains, HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'owner_id',
+        'name',
+        'logo',
+        'status',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'status' => TenantStatus::class,
+    ];
 
     protected $connection = 'central';
 
     public static function getCustomColumns(): array
     {
         return ['id', 'name', 'plan', 'status', 'created_at', 'updated_at', 'deleted_at'];
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
     }
 }
