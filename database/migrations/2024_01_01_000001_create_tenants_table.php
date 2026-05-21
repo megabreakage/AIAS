3d10809d6021dae862937d51e6b12dc71b383850
Martin <martin.njau@live.com>
2026-05-21 16:11:17 +0000

    🎨 refactor: enhance tenants table schema with additional fields and constraints

--- database/migrations/2024_01_01_000001_create_tenants_table.php
+++ database/migrations/2024_01_01_000001_create_tenants_table.php
@@ -13,8 +13,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::connection('central')->create('tenants', function (Blueprint $table): void {
-            $table->string('id')->primary();
+            $table->id(); // Primary Key, auto-increaments
+            $table->uuid('identifier')->unique()->index();
+            $table->foreignId('owner_id')->constrained('users')->restrictOnDelete(); // Must have an existing user with `tenant-admin` role assigned to them
+            $table->string('name')->unique()->index();
+            $table->string('domain')->nullable(); // if available it should be unique from existing domains
+            $table->string('logo')->nullable();
+            $table->foreignId('country_id')->nullable()->constrained('countries')->nullOnDelete();
+            $table->string('data_center')->nullable();
             $table->json('data')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
