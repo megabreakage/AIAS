0cc95bd64979190c24de2a3e793f43d6f600e6b0
Martin <martin.njau@live.com>
2026-05-22 20:41:59 +0000

    May 22, 2026, 11:41:59 PM

---
+++ app/app/Console/Commands/DropDatabases.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\app\Console\Commands;
+
+class DropDatabases
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
